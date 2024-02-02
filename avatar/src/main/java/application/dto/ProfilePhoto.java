ffbfc067fc3bb47457de003d46475f5fea3b5839
Rafael Santos <rafa.05125@gmail.com>
2024-02-01 23:03:32 -0300

    CustomerDTO, ProfilePhotoDTO

---
+++ avatar/src/main/java/application/dto/ProfilePhoto.java
@@ -0,0 +1,18 @@
+package application.dto;
+
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+
+import java.util.UUID;
+
+public record ProfilePhoto(FileUpload fileUpload) {
+    public domain.models.ProfilePhoto toDomain() {
+        return new domain.models.ProfilePhoto
+                (UUID.randomUUID().toString(),
+                        fileUpload().uploadedFile().toAbsolutePath().toString(),
+                        null);
+    }
+
+    public static ProfilePhoto create(FileUpload fileUpload) {
+        return new ProfilePhoto(fileUpload);
+    }
+}
